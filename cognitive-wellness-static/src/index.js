5ffa23a2af9960328aab9652fcf33ebfcd47e21b
Nathan Howard <howardn428@gmail.com>
2026-08-16 19:40:33 -0700

    email2

--- cognitive-wellness-static/src/index.js
+++ cognitive-wellness-static/src/index.js
@@ -23,7 +23,7 @@ export default {
 
                 const { data, error } = await resend.emails.send({
                     from: "Cognitive Wellness Solutions <website@cognitivewellnesssolutions.com>",
-                    to: ["howardn428@gmail.com"],
+                    to: ["jennifer@cognitivewellnesssolutions.com"],
                     replyTo: email,
                     subject: `Website Contact Form — ${name}`,
                     text: `
